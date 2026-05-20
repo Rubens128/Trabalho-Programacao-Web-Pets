126064b7d52809d4e7989c545063abdbabde022c
Rubens de Oliveira Cristiano <rubens.cristiano@a.ucb.br>
2026-05-19 23:33:29 -0300

    feat(login): Criação da pagina de login

--- frontend/src/components/Input/input.js
+++ frontend/src/components/Input/input.js
@@ -1,15 +1,17 @@
 import styles from "./input.module.css"
 
 function InputComponent({variavel, funcaoSetVariavel, icone: Icon = null, sizeIcon = 20,
-    placeholder = "", width = "50%", height = "5.5dvh", type = "text"}){
+    placeholder = "", width = "50%", height = "5.5dvh", type = "text", id = null}){
 
     return(
         <div className={styles.divInput} style={{width: width, height: height}}>
             {Icon ? <Icon size={sizeIcon} className={styles.divInputIcon}/> : ""}
+            
             <input className={styles.input} placeholder={placeholder} value={variavel} type={type}
-            style={{padding: Icon ? "5px 10px 5px 40px" : "5px 10px"}}
+            style={{padding: Icon ? "5px 10px 5px 40px" : "5px 10px"}} id={id}
             onChange={funcaoSetVariavel ? (e) => funcaoSetVariavel(e.currentTarget.value) : (e) => {return;}}>
             </input>
+
         </div>
 
     );
--- frontend/src/pages/Login/login.js
+++ frontend/src/pages/Login/login.js
@@ -1,9 +1,55 @@
-import Header from "../../components/Header/header.js"
+import styles from "./login.module.css";
+import Header from "../../components/Header/header.js";
+import InputComponent from "../../components/Input/input.js";
+import ButtonComponent from "../../components/Button/button.js";
+import LogoImage from "../../assets/logo_pets.svg";
+import { useState } from "react";
+import { FaUser } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+import { FaPaw } from "react-icons/fa";
 
 function Login(){
+
+    const [emailInputValue, setEmailInputValue] = useState("");
+    const [senhaInputValue, setSenhaInputValue] = useState("");
+
     return (
         <div>
             <Header/>
+            <div className={styles.divPrincipal}>
+                <div className={styles.divPrincipalLogo}>
+                    <img src={LogoImage} alt="Imagem da Logo do site"/>
+                    <h1><span style={{color: "#d6a559"}}>Exo</span>Pets</h1>
+                    <p>Conexão exótica | Amor que Transforma </p>
+                </div>
+                <div className={styles.divPrincipalDivLogin}>
+                    
+                    <div className={styles.divPrincipalDivLoginTitulo}>
+                        <h1>Bem-Vindo(a) de volta!</h1>
+                        <p>Faça login para continuar</p>
+                    </div>
+
+                    <div className={styles.divPrincipalDivLoginInput}>
+                        <label>E-mail</label>
+                        <InputComponent variavel={emailInputValue} funcaoSetVariavel={setEmailInputValue}
+                        icone={FaUser} width="100%" type="email" placeholder="seu@email.com"/>
+                    </div>
+                    
+                    <div className={styles.divPrincipalDivLoginInput}>
+                        <label for="email">Senha</label>
+                        <InputComponent variavel={senhaInputValue} funcaoSetVariavel={setSenhaInputValue}
+                        icone={FaLock} width="100%" type="password" placeholder="Digite sua senha" id="email"/>
+                        <a href="/">Esqueceu sua senha?</a>
+                    </div>
+
+                    <div className={styles.divPrincipalDivBotoes}>
+                        <ButtonComponent textoBotao="Entrar" icone={FaPaw} variante={1} width="100%" height="8dvh"/>
+                        <p>ou</p>
+                        <ButtonComponent textoBotao="Criar uma conta" icone={FaPaw} variante={2} width="100%" height="7dvh"/>
+                    </div>
+                    
+                </div>
+            </div>
         </div>
     );
 }
