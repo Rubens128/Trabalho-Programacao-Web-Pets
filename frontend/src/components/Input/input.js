dc8004d3e0fc7bfff0197f261eeb6197e11a7265
Rubens de Oliveira Cristiano <rubens.cristiano@a.ucb.br>
2026-05-19 22:19:39 -0300

    feat(input): Criação do componente de input

---
+++ frontend/src/components/Input/input.js
@@ -0,0 +1,18 @@
+import styles from "./input.module.css"
+
+function InputComponent({variavel, funcaoSetVariavel, icone: Icon = null, sizeIcon = 20,
+    placeholder = "", width = "50%", height = "5.5dvh", type = "text"}){
+
+    return(
+        <div className={styles.divInput} style={{width: width, height: height}}>
+            {Icon ? <Icon size={sizeIcon} className={styles.divInputIcon}/> : ""}
+            <input className={styles.input} placeholder={placeholder} value={variavel} type={type}
+            style={{padding: Icon ? "5px 10px 5px 40px" : "5px 10px"}}
+            onChange={funcaoSetVariavel ? (e) => funcaoSetVariavel(e.currentTarget.value) : (e) => {return;}}>
+            </input>
+        </div>
+
+    );
+}
+
+export default InputComponent;
